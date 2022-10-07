e38e6540035034004781e2c6e04b927f04c2d238
omobosteven <omobosteven@gmail.com>
2022-10-07 14:49:20 +0100

    Add error boundary

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "core-js/stable";
 import "./styles.css";
+import DefaultErrorBoundary from "./DefaultErrorBoundary";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>
+);
