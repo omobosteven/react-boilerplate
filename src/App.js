02562aed5026bab1b594650a9d4e9988cd7ff595
omobosteven <omobosteven@gmail.com>
2022-10-07 15:00:24 +0100

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, { useState } from "react";
-
-const Warning = React.lazy(() => import("./Warning"));
+import React from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <h1>Hello world!</h1>
-      <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
-      <button onClick={() => setCount((state) => state + 1)}>+</button>
-      <button onClick={() => setCount((state) => state - 1)}>-</button>
-      {count > 10 ? (
-        <React.Suspense fallback={null}>
-          <Warning />
-        </React.Suspense>
-      ) : null}
     </div>
   );
 };
