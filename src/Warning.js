9e461fccd4cf0efdab2e80601fe777fea7d0489a
omobosteven <omobosteven@gmail.com>
2022-10-07 14:37:20 +0100

    Add eslint and husky

--- src/Warning.js
+++ src/Warning.js
@@ -1,3 +1,5 @@
 import React from "react";
 
-export default () => <span className={"warning"}>Take it easy!</span>;
+const Warning = () => <span className={"warning"}>Take it easy!</span>;
+
+export default Warning;
